d53e1a02df2f3a26254e76592ca9084a8cd8d78b
Quorafind <bonopengate@gmail.com>
2022-05-27 12:02:46 +0800

    First Release

--- main.ts
+++ main.ts
@@ -1,85 +1,79 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Menu, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { foldable } from "@codemirror/language";
+import { EditorState } from "@codemirror/state";
+import { EditorView } from "@codemirror/view";
+import { createNewMDNote } from "obsidian-community-lib"
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface TransThemSettings {
+	removeFirstLine: boolean;
+	keepOriginalText: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: TransThemSettings = {
+	removeFirstLine: false,
+	keepOriginalText: true,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TransThemPlugin extends Plugin {
+	settings: TransThemSettings;
 
 	async onload() {
+		this.addSettingTab(new TransThemSettingTab(this.app, this));
 		await this.loadSettings();
+		this.app.workspace.onLayoutReady(this.onLayoutReady.bind(this));
+		
+		this.registerEvent(
+			this.app.workspace.on('editor-menu', (menu: Menu, editor: Editor, view: MarkdownView) => {
+			  const range = editor.getCursor();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			  const beginRange = {
+				  ch: 0,
+				  line: range.line,
+			  }
+			  // Check if the cursor is at the bullet point
+			  const newRange = {
+				  ch: range.ch + 1,
+				  line: range.line,
+			  }
+			  const bulletString = editor.getRange(beginRange , newRange);
+			  if(!(/^\s*[-\*\+]\s?$/.test(bulletString))) {
+				  return;
+			  }
+			  menu.addItem((item) =>
+                  item
+                    .setIcon("hash")
+                    .setTitle(`Transfer bullet to note`)
+                    .onClick(async () => {
+						await this.createNoteWithBulletContent(editor, this.settings.removeFirstLine);
+						// eslint-disable-next-line @typescript-eslint/no-explicit-any
+						// const view: EditorView = (editor as any).cm;
+                        // console.log(`Transform bullet to note`);
+					    // const offset = editor.posToOffset(newRange);
+						// const line = view.state.doc.lineAt(offset);
+					    // const lineRange = this.calculateRangeForTransform(view.state, offset);
+						// if(!lineRange) {
+						// 	return;
+						// }
+						// // const regexForTabAndSpace = this.shouldReplaceTextRegex(line.text);
+						// const tokenForTabAndSpace = this.shouldReplaceToken(line.text);
+						// const text = editor.getRange(editor.offsetToPos(lineRange.from), editor.offsetToPos(lineRange.to));
+						// const content = this.shouldInsertContent(text, tokenForTabAndSpace);
+						
+						// // console.log(regexForTabAndSpace);
+						// // const content = editor.offsetToPos(lineRange.from).line != editor.offsetToPos(lineRange.to).line ? editor.getRange(editor.offsetToPos(lineRange.from), editor.offsetToPos(lineRange.to)).replace(/^\s*(?:([-*+]|\d+\.))/,"$1") : editor.getRange(editor.offsetToPos(lineRange.from), editor.offsetToPos(lineRange.to));
+						// const createdFile = await createNewMDNote(line?.text.replace(/^\s*([-*+]|\d+\.)\s+/g,""), app.workspace.getActiveFile()?.path);
+						// app.vault.modify(createdFile, content);
+						// app.workspace.getLeaf(true).openFile(createdFile);
+                    })
+                );
+			}),
+		  );
 	}
 
 	onunload() {
-
+		
 	}
 
 	async loadSettings() {
@@ -89,49 +83,185 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async onLayoutReady(): Promise<void> {
+		this.addCommand({
+			id: 'transform-bullet-into-note',
+			name: 'Transform bullet into note with first line',
+			editorCallback: (editor: Editor) => {
+				this.createNoteWithBulletContent(editor, this.settings.removeFirstLine);
+			},
+		});
+	}
+
+	public async createNoteWithBulletContent(editor: Editor, removeFirstLine: boolean) {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const view: EditorView = (editor as any).cm;
+		console.log(`Transform bullet into note`);
+		const range = editor.getCursor();
+		const currentLine = editor.getLine(range.line);
+		if(!(/^\s*[-\*\+]\s+/.test(currentLine))) {
+			return;
+		}
+		const lineRange = this.calculateRangeForTransform(view.state, editor.posToOffset(range));
+
+		if(!lineRange) {
+			return;
+		}
+		const tokenForTabAndSpace = this.shouldReplaceToken(currentLine);
+		const text = editor.getRange(editor.offsetToPos(lineRange.from), editor.offsetToPos(lineRange.to));
+		if(!this.settings.keepOriginalText) {
+			// Temporary remove the bullet
+			const titleText = currentLine.replace(/^(\s*[-\*\+]\s+)/,"");
+			const linkText = titleText.replace(/(.*)?/, "[[$1]]");
+
+			// Get the begin and end of the range
+			const contentBeginPositions = editor.offsetToPos(lineRange.from);
+			const contentEndPositions = editor.offsetToPos(lineRange.to);
+
+			editor.replaceRange(currentLine.replace(titleText, linkText), contentBeginPositions, contentEndPositions);
+		} 
+		const content = this.shouldInsertContent(text, tokenForTabAndSpace, removeFirstLine);
+		
+		const createdFile = await createNewMDNote(currentLine?.replace(/^\s*([-*+]|\d+\.)\s+/g,""), app.workspace.getActiveFile()?.path);
+		app.vault.modify(createdFile, content);
+		app.workspace.getLeaf(true).openFile(createdFile);
+	}
+
+	public shouldInsertContent(text: string, token: string | null, removeFirstLine: boolean): string {
+		let tempContent: string = "";
+		const spiltContentByLineBreak = text.split(/\r?\n/);
+		if(token) {
+			for(let i = 0; i < spiltContentByLineBreak.length; i++) {
+				if(!i) {
+				 	if(!removeFirstLine) { 
+						tempContent = spiltContentByLineBreak[0].replace(token, '');
+					}
+					continue;
+				}
+				
+				if(removeFirstLine) {
+					tempContent += (i == 1 ? `${spiltContentByLineBreak[i].replace(token, '').replace(token[0], '')}` : `\n${spiltContentByLineBreak[i].replace(token, '').replace(token[0], '')}`);
+				}else{
+					tempContent += `\n${spiltContentByLineBreak[i].replace(token, '')}`;
+				}
+			}
+			return tempContent;
+		}else {
+			if(!removeFirstLine) {
+				return text;
+			}
+
+			// No Token But Remove first line
+			let tempToken;
+			if(spiltContentByLineBreak.length > 1) {
+				tempToken = this.shouldReplaceToken(spiltContentByLineBreak[1]);
+			}
+
+			console.log(tempToken);
+			for(let i = 0; i < spiltContentByLineBreak.length; i++) {
+				if(!i) {
+				 	if(!removeFirstLine) { 
+						tempContent = spiltContentByLineBreak[0].replace(tempToken, '');
+					}
+					continue;
+				}
+				
+				if(removeFirstLine) {
+					tempContent += (i == 1 ? `${spiltContentByLineBreak[i].replace(tempToken, '').replace(tempToken[0] === ' '? '' : '\t', '')}` : `\n${spiltContentByLineBreak[i].replace(tempToken, '').replace(tempToken[0] === ' '? '' : '\t', '')}`);
+				}else{
+					tempContent += `\n${spiltContentByLineBreak[i].replace(tempToken, '')}`;
+				}
+			}
+			return tempContent;
+		}
+	}
+
+	public shouldReplaceToken(text: string): string {
+		const beginWithTab = text.startsWith('\t');
+		const beginWithSpace = text.startsWith(' ');
+		if(!beginWithSpace && !beginWithTab) {
+			return null;
+		}
+		const tabAndSpace = text.match(/^\s*/g)[0];
+		return tabAndSpace;
+	}
+
+	// public shouldReplaceTextRegex(text: string): RegExp {
+	// 	const beginWithTab = text.startsWith('\t');
+	// 	const beginWithSpace = text.startsWith(' ');
+	// 	let tokenToBeReplaced;
+	// 	if(!beginWithSpace && !beginWithTab) {
+	// 		return null;
+	// 	}
+	// 	const tabAndSpace = text.match(/^\s*/g)[0];
+	// 	const countTabAndSpace = (tabAndSpace.match(/\t| /g) || []).length;
+	// 	if(app.vault.getConfig('useTab') && beginWithTab) {
+	// 		tokenToBeReplaced = '\\t\{' + countTabAndSpace + '\}'; 
+	// 	}
+	// 	if(beginWithSpace) {
+	// 		tokenToBeReplaced = '\\s\{' + countTabAndSpace + '\}';
+	// 	}
+		
+	// 	return new RegExp(tokenToBeReplaced, 'g'); 
+	// }
+
+	public calculateRangeForTransform(state: EditorState, pos: number) {
+		const line = state.doc.lineAt(pos);
+		const foldRange = foldable(state, line.from, line.to);
+
+		if (!foldRange && /^\s*([-*+]|\d+\.)\s+/.test(line.text)) {
+			return { from: line.from, to: line.to };
+		}
+
+		if (!foldRange) {
+			return null;
+		}
+
+		return { from: line.from, to: foldRange.to };
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class TransThemSettingTab extends PluginSettingTab {
+	plugin: TransThemPlugin;
+	private applyDebounceTimer: number = 0;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: TransThemPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
+
+	applySettingsUpdate() {
+		clearTimeout(this.applyDebounceTimer);
+		const plugin = this.plugin;
+		this.applyDebounceTimer = window.setTimeout(() => {
+		  plugin.saveSettings();
+		}, 100);
+	  }
 
 	display(): void {
 		const {containerEl} = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Menu Them'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
+			.setName('Remove First Line when Create Note')
+			.setDesc('remove first bullet line when create new note')
+			.addToggle((toggle) =>
+				toggle.setValue(this.plugin.settings.removeFirstLine).onChange(async (value) => {
+					this.plugin.settings.removeFirstLine = value;
+					this.applySettingsUpdate();
+        		}));
+		
+		new Setting(containerEl)
+			.setName('Keep Original Text')
+			.setDesc('keep original text when create new note')
+			.addToggle((toggle) =>
+				toggle.setValue(this.plugin.settings.keepOriginalText).onChange(async (value) => {
+					this.plugin.settings.keepOriginalText = value;
+					this.applySettingsUpdate();
 				}));
 	}
 }
